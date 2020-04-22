dd2ecfcee68e68e5a6d61fd72873522fbb7b166b
Kyle Awayan <kyle@awayan.com>
2020-04-22 13:02:17 -0700

    add test to make sure webpage runs sucessfully

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ http.createServer(function(request, response) {
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+})
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+console.log("Static file server running.");
---
+++ app.test.js
@@ -0,0 +1,5 @@
+const web = require('./app');
+
+test('check if webpage is running', () => {
+    expect('Static file server running.')
+})
