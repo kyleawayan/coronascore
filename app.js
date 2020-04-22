d47a7bc1d3b33a99af9410796c6881c049cb4ad9
Kyle Awayan <kyle@awayan.com>
2020-04-21 22:55:25 -0700

    prototype page

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-const http = require('http')
-const fs = require('fs')
-const port = 8081
+// from aolde. thank you! https://gist.github.com/aolde/8104861
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-    res.end()
-    })
-})
-
-server.listen(port, function(error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    } else {
-        console.log('Server is listening on port ' + port)
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8081,
+    mimeTypes = {
+      "html": "text/html",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "png": "image/png",
+      "js": "text/javascript",
+      "css": "text/css"
+    };
+ 
+http.createServer(function(request, response) {
+ 
+  var uri = url.parse(request.url).pathname, 
+      filename = path.join(process.cwd(), uri);
+  
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not Found\n");
+      response.end();
+      return;
     }
-})
+ 
+    if (fs.statSync(filename).isDirectory()) 
+      filename += 'index.html';
+ 
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+      
+      var mimeType = mimeTypes[filename.split('.').pop()];
+      
+      if (!mimeType) {
+        mimeType = 'text/plain';
+      }
+      
+      response.writeHead(200, { "Content-Type": mimeType });
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
