ee810f90b7b7dfc3cabc95d0bd831200075e329f
Gabe-H <gabe.haarberg@gmail.com>
2020-04-23 04:08:00 -0700

    Reworked to use nytimes data

---
+++ python_scripts/github_read.py
@@ -0,0 +1,35 @@
+# Call with file.py <State> <County>
+# if county is 'listall', temp file will show all counties
+
+import pandas as pd
+import os
+from pathlib import Path
+import sys
+
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+data_folder = Path('csvs')
+path = os.path.join(data_folder,'covid_19_counties.csv')
+
+df = pd.read_csv(url, error_bad_lines=False)
+
+df.to_csv(path, index=True)
+print('CSV Created!')
+
+## Uncomment to 'incorporate' csv_read
+#
+## myState = sys.argv[1]
+## myCounty = sys.argv[2]
+## 
+## if myCounty == 'listall':
+##     df = df.loc[df['state'] == myState]
+##     df.reset_index(drop=True, inplace=True)
+##     df = df.drop(columns=['state', 'fips'])
+##     print('Check temp.csv!')
+## else:
+##     df = df.loc[(df['county'] == myCounty) & (df['state'] == myState)]
+##     df.reset_index(drop=True, inplace=True)
+##     df = df.drop(columns=['county', 'state', 'fips'])
+##     result = pd.concat([df.tail(1)])
+##     print(f'\nLatest data:\n{result}\n')
+## 
+## df.to_csv(os.path.join(data_folder,'temp.csv'))
