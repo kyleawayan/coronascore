55b53de6851f9bab7cdbbe2b1da78db54ebf0247
Kyle Awayan <kyle@awayan.com>
2020-04-23 23:20:16 -0700

    animations

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
 function gec() {
   var geocoder = new google.maps.Geocoder();
   var address = document.getElementsByName("location")[0].value;
+  let center = document.querySelector('.center');
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       console.log(results)
-      element.classList.add("animation");
+      center.classList.add('animation');
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
