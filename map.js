17f806e415cd67d2ee30ed867626628b37f7a214
Gabe-H <gabe.haarberg@gmail.com>
2020-04-25 01:27:27 -0700

    error for objects in multiple states (lake tahoe)

--- map.js
+++ map.js
@@ -9,48 +9,55 @@ export function gec() {
     if (status === 'OK') {
       var arrayRes = JSON.stringify((results[0])['address_components']).split(':');
       var foundB = arrayRes.indexOf('["administrative_area_level_1","political"]},{"long_name"');
-      var stateName = (arrayRes[foundB-2].split(','))[0];
-      var STATE_NAME = stateName.slice(1, -1);
+      if (foundB == -1) {
+        alert('Please enter a more specific location!')
+      }
+      else  {
+        var stateName = (arrayRes[foundB-2].split(','))[0];
+        var STATE_NAME = stateName.slice(1, -1);
 
-      if (STATE_NAME == 'New York') {
-        console.log(results)
-        if (arrayRes.includes('["administrative_area_level_2","political"]},{"long_name"') == false)  {
-          COUNTY_NAME = 'New York City'
-          countyResults(COUNTY_NAME, STATE_NAME);
+        if (STATE_NAME == 'New York') {
+          console.log(results)
+          if (arrayRes.includes('["administrative_area_level_2","political"]},{"long_name"') == false)  {
+            COUNTY_NAME = 'New York City'
+            countyResults(COUNTY_NAME, STATE_NAME);
+          }
+          else  {
+            var foundA = arrayRes.indexOf('["administrative_area_level_2","political"]},{"long_name"');
+            var countyName = (arrayRes[foundA-1].split(','))[0];
+
+            if (countyName.slice(1, -1) == 'New York County' || countyName.slice(1, -1) == 'Bronx County'){
+              COUNTY_NAME = 'New York City'
+            }
+            else if (countyName.slice(1, -1) == 'Queens County' || countyName.slice(1, -1) == 'Kings County')  {
+              COUNTY_NAME = 'New York City'
+            }
+            else {
+              COUNTY_NAME = countyName.slice(1, -8)
+            }
+
+            countyResults(COUNTY_NAME, STATE_NAME)
+          }
         }
-        else  {
+        else {
           var foundA = arrayRes.indexOf('["administrative_area_level_2","political"]},{"long_name"');
           var countyName = (arrayRes[foundA-1].split(','))[0];
 
-          if (countyName.slice(1, -1) == 'New York County' || countyName.slice(1, -1) == 'Bronx County'){
-            COUNTY_NAME = 'New York City'
+          if (countyName.endsWith('County"') == true) {
+            var COUNTY_NAME = countyName.slice(1, -8)
           }
-          else if (countyName.slice(1, -1) == 'Queens County' || countyName.slice(1, -1) == 'Kings County')  {
-            COUNTY_NAME = 'New York City'
+          else if (arrayRes.includes('["administrative_area_level_2","political"]},{"long_name"') == true) {
+            var COUNTY_NAME = countyName.slice(1, -1)
           }
           else {
-            COUNTY_NAME = countyName.slice(1, -8)
+            alert('Please enter a more specific location!')
           }
 
-          countyResults(COUNTY_NAME, STATE_NAME)
+          countyResults(COUNTY_NAME, STATE_NAME);
+          
+            }
         }
-      }
-      else {
-        var foundA = arrayRes.indexOf('["administrative_area_level_2","political"]},{"long_name"');
-        var countyName = (arrayRes[foundA-1].split(','))[0];
-
-        if (countyName.endsWith('County"') == true) {
-          var COUNTY_NAME = countyName.slice(1, -8)
-        }
-        else {
-          var COUNTY_NAME = countyName.slice(1, -1)
-        }
-
-        countyResults(COUNTY_NAME, STATE_NAME);
-        
-          }
-      }
-      
+    }
       
     else {
       alert('Geocode was not successful for the following reason: ' + status);
