02bea4fd4c7b2cdae68e38ffae2ff79e973c5c43
Kyle Awayan <kyle@awayan.com>
2020-04-25 01:52:58 -0700

    add cases count

--- countup.js
+++ countup.js
@@ -1,6 +1,8 @@
 import { CountUp } from './node_modules/countup.js/dist/countUp.min.js';
 let center = document.querySelector('.center');
 let score = document.querySelector('.score');
+let score2 = document.querySelector('.score2');
+let score3 = document.querySelector('.score3');
 
 const options = {
       separator: '',
@@ -13,6 +15,20 @@ export function startcount(SCORE) {
     score.classList.remove('hidden');
     score.classList.add('visible');
     var countUp = new CountUp('score', SCORE, options)
+    setTimeout(function() {
+        countUp.start();
+    }, 500);
+}
+
+export function info(cvData, C, D) {
+    let center = document.querySelector('.center');
+    let score = document.querySelector('.score');
+    center.classList.add('animation');
+    score2.classList.remove('hidden');
+    score2.classList.add('visible');
+    score3.classList.remove('hidden');
+    score3.classList.add('visible');
+    var countUp = new CountUp('info', C, options)
     setTimeout(function() {
         countUp.start();
     }, 500);
